f123925db04816f1cf9858f9b721791cc915f714
justfile <justfile@outlook.kr>
2019-03-17 18:26:28 +0900

    초기 커밋.

---
+++ FrontApp/ClientApp/src/components/Blog.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store/Counter';
+import {Editor, EditorState} from 'draft-js';
+
+// const Blog = props => (
+//   <div>
+//     <h1>Blog</h1>
+//   </div>
+// );
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  render() {
+    return (
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
+  }
+}
+
+export default connect()(Blog);
---
+++ Model/Data/YunazContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using Model.Models;
+
+namespace Model.Data
+{
+    public class YunazContext : DbContext
+    {
+        //public YunazContext(DbContextOptions<YunazContext> options) : base(options)
+        //{
+        //}
+
+        //public DbSet<Product> Products { get; set; }
+        ////public DbSet<Enrollment> Enrollments { get; set; }
+        ////public DbSet<Student> Students { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Product>().ToTable("Products");
+        //    //modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        //    //modelBuilder.Entity<Student>().ToTable("Student");
+        //}
+    }
+}
